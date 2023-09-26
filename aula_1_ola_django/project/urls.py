b5a82309714d5ed14f85d18815dbe6e6e58b1b6a
Dan <danielpinhodovale@gmail.com>
2023-09-25 23:02:22 -0300

    http_response

--- aula_1_ola_django/project/urls.py
+++ aula_1_ola_django/project/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_view(request):
+    print('Fazendo oq eu quero, essa mensagem vai aparecer no console')
+    return HttpResponse('Salve')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
 ]
